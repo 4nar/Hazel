5b0809c1169c33789c7f313776ce270ab46f7324
Anar ABETAYEVA <anar.abetayeva@amadeus.com>
2025-04-09 19:37:04 +0200

    Setup basic application and Entry point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// For use by Hazel applications
+
+
+#include "Hazel/Application.h"
+
+
+// -----------EntryPOint-----------
+#include "Hazel/EntryPoint.h"
+// -------------------------------
+
